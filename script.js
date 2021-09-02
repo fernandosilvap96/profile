288014c2e2367402d9e4d1578ff743bb9d068d5c
Fernando da Silva Pinto <fernando-p_@outlook.com>
2021-09-02 14:57:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,17 +45,6 @@ function showSection(element){
           document.querySelector("#" + target).classList.add("active")
 }
 
-/*Tradução*/
-<script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'pt', includedLanguages: 'en,pt', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
-}
-</script>
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-
-
-
 // Mini Menu responsivo
 
 const miniMenu = document.querySelector(".mini-menu-button"), 
