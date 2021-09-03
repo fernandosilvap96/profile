36c49fea05b2e813f359ca5b9259de5b12e8705d
Fernando da Silva Pinto <fernando-p_@outlook.com>
2021-09-03 11:37:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,25 +47,48 @@ function showSection(element){
 
 
 
-//Titulo Fernando
-var byline = document.getElementById('byline');  	
-bylineText = byline.innerHTML;										
-bylineArr = bylineText.split('');									
-byline.innerHTML = '';														
+//Titulo da pagina HOME
 
-var span;					
-var letter;
+let words = document.querySelectorAll(".word");
+words.forEach(word => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach(letter => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
+});
 
-for(i=0;i<bylineArr.length;i++){								
-  span = document.createElement("span");				
-  letter = document.createTextNode(bylineArr[i]);	
-  if(bylineArr[i] == ' ') {											
-    byline.appendChild(letter);					
-  } else {
-		span.appendChild(letter);						
-  	byline.appendChild(span); 					
-  }
-}
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+(words[currentWordIndex] as HTMLElement).style.opacity = "1";
+
+let rotateText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+  // rotate out letters of current word
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  // reveal and rotate in letters of next word
+  (nextWord as HTMLElement).style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+rotateText();
+setInterval(rotateText, 4000);
 
 
 
