f5ca875696bc7c987346e8bc98aabde8184bd2d1
Fernando da Silva Pinto <fernando-p_@outlook.com>
2021-09-03 11:15:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,33 @@ function showSection(element){
           document.querySelector("#" + target).classList.add("active")
 }
 
+
+
+//Titulo Fernando
+var byline = document.getElementById('byline');  	
+bylineText = byline.innerHTML;										
+bylineArr = bylineText.split('');									
+byline.innerHTML = '';														
+
+var span;					
+var letter;
+
+for(i=0;i<bylineArr.length;i++){								
+  span = document.createElement("span");				
+  letter = document.createTextNode(bylineArr[i]);	
+  if(bylineArr[i] == ' ') {											
+    byline.appendChild(letter);					
+  } else {
+		span.appendChild(letter);						
+  	byline.appendChild(span); 					
+  }
+}
+
+
+
+
+
+
 // Mini Menu responsivo
 
 const miniMenu = document.querySelector(".mini-menu-button"), 
